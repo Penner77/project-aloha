fb7fcbb48247e3d745d278712f5c24260081736d
Gay Stephenson <gaystephenson@me.com>
2019-04-19 17:47:03 -0700

    Added js main file and in class work on carousel and clickable links

---
+++ main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() { 
+
+
+    console.log('this is working');
+
+
+ const flkty = new Flickity( '.main-carousel', {
+    cellAlign: 'left',
+    wrapAround: true,
+    freeScroll: true
+  });
+
+});// end of document ready
